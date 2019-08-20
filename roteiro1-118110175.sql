83418ee2ca5a1581f48cf2107fe29863ea356ab4
Luis Mafra <luis.mafra@ccc.ufcg.edu.br>
2019-08-20 14:53:16 -0300

    pronto

--- roteiro1-118110175.sql
+++ roteiro1-118110175.sql
@@ -29,7 +29,6 @@ CREATE TABLE oficina(
 
 CREATE TABLE seguro(
 	id_seguro INTEGER,
-	empresa_seguro TEXT,
 	cpf_segurado TEXT,
 	id_sinistro INTEGER,
 	inicio_seguro date,
@@ -65,6 +64,7 @@ ALTER TABLE seguro ADD PRIMARY KEY(id_seguro);
 ALTER TABLE sinistro ADD PRIMARY KEY(id_sinistro);
 ALTER TABLE pericia ADD PRIMARY KEY(id_pericia);
 ALTER TABLE reparo ADD PRIMARY KEY(id_reparo);
+
 ALTER TABLE automovel ADD CONSTRAINT chave_estrangeira FOREIGN KEY(cpf_segurado) REFERENCES segurado(cpf_segurado) ON DELETE SET NULL ON UPDATE CASCADE;
 ALTER TABLE pericia ADD CONSTRAINT chave_estrangeira FOREIGN KEY(cpf_perito) REFERENCES perito(cpf_perito) ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE pericia ADD CONSTRAINT chave_estrangeira_2 FOREIGN KEY(placa_automovel) REFERENCES automovel(placa_automovel) ON UPDATE CASCADE ON DELETE SET NULL;
@@ -74,3 +74,87 @@ ALTER TABLE reparo ADD CONSTRAINT chave_estrangeira_3 FOREIGN KEY(id_pericia) RE
 ALTER TABLE reparo ADD CONSTRAINT chave_estrangeira_4 FOREIGN KEY(id_seguro) REFERENCES seguro(id_seguro) ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE seguro ADD CONSTRAINT chave_estrangeira FOREIGN KEY(cpf_segurado) REFERENCES segurado(cpf_segurado) ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE seguro ADD CONSTRAINT chave_estrangeira_2 FOREIGN KEY(id_sinistro) REFERENCES sinistro(id_sinistro) ON UPDATE CASCADE ON DELETE SET NULL;
+
+DROP TABLE reparo;
+DROP TABLE seguro;
+DROP TABLE sinistro;
+DROP TABLE oficina;
+DROP TABLE pericia;
+DROP TABLE perito;
+DROP TABLE automovel;
+DROP TABLE segurado;
+
+CREATE TABLE segurado(
+	nome_segurado TEXT NOT NULL,
+	cpf_segurado CHAR(11) NOT NULL,
+	endereco_segurado TEXT NOT NULL,
+	idade_segurado INTEGER NOT NULL,
+	telefone_segurado VARCHAR(15),
+	PRIMARY KEY(cpf_segurado)
+);
+
+CREATE TABLE automovel(
+	marca_automovel TEXT NOT NULL,
+	placa_automovel CHAR(7) NOT NULL,
+	cpf_segurado CHAR(11) NOT NULL REFERENCES segurado(cpf_segurado) ON DELETE SET NULL ON UPDATE CASCADE,
+	PRIMARY KEY(placa_automovel)
+);
+
+CREATE TABLE oficina(
+	nome_oficina TEXT NOT NULL,
+	endereco_oficina TEXT NOT NULL,
+	telefone_oficina VARCHAR(15),
+	PRIMARY KEY(nome_oficina)
+	
+);
+
+CREATE TABLE perito(
+	nome_perito TEXT NOT NULL,
+	cpf_perito CHAR(11) NOT NULL,
+	idade_perito INTEGER NOT NULL,
+	endereco_perito TEXT NOT NULL,
+	telefone_perito VARCHAR(15),
+	PRIMARY KEY(cpf_perito)
+);
+
+CREATE TABLE sinistro(
+	id_sinistro INTEGER NOT NULL,
+	tipo_sinistro TEXT NOT NULL,
+	PRIMARY KEY(id_sinistro)
+);
+
+CREATE TABLE seguro(
+	id_seguro INTEGER NOT NULL,
+	cpf_segurado TEXT NOT NULL REFERENCES segurado(cpf_segurado) ON UPDATE CASCADE ON DELETE SET NULL,
+	id_sinistro INTEGER NOT NULL REFERENCES sinistro(id_sinistro) ON UPDATE CASCADE ON DELETE SET NULL,
+	inicio_seguro date NOT NULL,
+	fim_seguro date,
+	PRIMARY KEY(id_seguro)
+);
+
+CREATE TABLE pericia(
+	status_pericia boolean,
+	id_pericia INTEGER NOT NULL,
+	cpf_perito VARCHAR(11) NOT NULL REFERENCES perito(cpf_perito) ON UPDATE CASCADE ON DELETE SET NULL,
+	placa_automovel CHAR(7) NOT NULL REFERENCES automovel(placa_automovel) ON UPDATE CASCADE ON DELETE SET NULL,
+	PRIMARY KEY(id_pericia)
+);
+
+CREATE TABLE reparo(
+	id_reparo INTEGER,
+	status_reparo boolean,
+	placa_automovel CHAR(7) REFERENCES automovel(placa_automovel) ON UPDATE CASCADE ON DELETE SET NULL,
+	nome_oficina TEXT REFERENCES oficina(nome_oficina) ON UPDATE CASCADE ON DELETE SET NULL,
+	id_pericia INTEGER REFERENCES pericia(id_pericia) ON UPDATE CASCADE ON DELETE SET NULL,
+	id_seguro INTEGER REFERENCES seguro(id_seguro) ON UPDATE CASCADE ON DELETE SET NULL,
+	PRIMARY KEY(id_reparo)
+);
+
+DROP TABLE reparo;
+DROP TABLE seguro;
+DROP TABLE sinistro;
+DROP TABLE oficina;
+DROP TABLE pericia;
+DROP TABLE perito;
+DROP TABLE automovel;
+DROP TABLE segurado;
