7d79a0e48da2b10845f3f67875b1ea45d6ef49f0
luismafra <luis.mafra@ccc.ufcg.edu.br>
2019-09-22 18:54:47 -0300

    update

--- roteiro5-luis-118110175.sql
+++ roteiro5-luis-118110175.sql
@@ -8,3 +8,22 @@ SELECT AVG(salary)
 FROM employee
 WHERE sex = 'M' AND address LIKE '%TX';
 
+--Q3
+SELECT f.superssn as ssn_supervisor, COUNT(*) as qtd_supervisionados
+FROM (EMPLOYEE as e RIGHT OUTER JOIN EMPLOYEE as f ON e.ssn = f.superssn)
+GROUP BY f.superssn
+ORDER BY COUNT(*);
+
+--Q4
+SELECT e.fname as nome_supervisor, COUNT(*) as qtd_supervisionados
+FROM (EMPLOYEE as e JOIN EMPLOYEE as f ON e.ssn = f.superssn)
+GROUP BY e.fname
+ORDER BY COUNT(*);
+
+--Q5
+SELECT e.fname as nome_supervisor, COUNT(*) as qtd_supervisionados
+FROM (EMPLOYEE as e RIGHT OUTER JOIN EMPLOYEE as f ON e.ssn = f.superssn)
+GROUP BY e.fname
+ORDER BY COUNT(*);
+
+--Q6
